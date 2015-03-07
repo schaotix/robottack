0e153c003b7569058497bd0a6f560f4d52bd9e6d
schaotix <screamer.chaotix.24@gmail.com>
2015-03-07 10:32:40 -0500

    first commit

---
+++ attack.py
@@ -0,0 +1,29 @@
+import time
+
+class Target:
+    power = 5
+    def attack(self):
+        print('Gah!')
+        self.power -= 1
+
+    def checkPower(self):
+        if self.power <= 0:
+            print('\x1b[45m')
+            print('D E S T R O Y E D')
+            print('\x1b[m')
+        else:
+            print('Power at {}'.format(self.power))
+
+robo1 = Target()
+num = int(input('Fire how many shots? '))
+while num != 0:
+    print('\x1b[31m')
+    print('Firing!')
+    time.sleep(1)
+    print('\x1b[m')
+    robo1.attack()
+    num -= 1
+    robo1.checkPower()
+
+
+
